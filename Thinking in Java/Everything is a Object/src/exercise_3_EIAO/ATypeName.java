46a824925249bb965545e6c256ff7c772aeaebed
Edgar Hernandez <riseofagamer@gmail.com>
2018-12-18 15:53:17 -0300

    Thinking in Java, Everything is a Object, Exercise 3

---
+++ Thinking in Java/Everything is a Object/src/exercise_3_EIAO/ATypeName.java
@@ -0,0 +1,9 @@
+package exercise_3_EIAO;
+
+public class ATypeName 
+{
+	public ATypeName()
+	{
+		System.out.print("I've just borned");
+	}
+}
---
+++ Thinking in Java/Everything is a Object/src/exercise_3_EIAO/Exercise3.java
@@ -0,0 +1,10 @@
+package exercise_3_EIAO;
+
+public class Exercise3 
+{
+	public static void main(String[] args) 
+	{
+		ATypeName testClass = new ATypeName();
+	}
+
+}
