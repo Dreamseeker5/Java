978822bbb4f4b3c8ec9400ec1e1fdea29603f9bd
Edgar Hernandez <riseofagamer@gmail.com>
2018-12-19 16:58:09 -0300

    Thinking in Java, Controlling Execution, Exercise 8

---
+++ Thinking in Java/Controlling Execution/src/exercise_8_CE/Exercise8.java
@@ -0,0 +1,25 @@
+package exercise_8_CE;
+
+public class Exercise8 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			switch (i) 
+			{
+			case 0:
+				System.out.println("Another one");
+				break;
+			case 1:
+				System.out.println("bite of");
+				break;
+			case 2:
+				System.out.println("dust");
+				break;
+			}
+		}
+
+	}
+
+}
