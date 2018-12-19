5c77ec23cceda7075087a79385f8c64d6ee1529b
Edgar Hernandez <riseofagamer@gmail.com>
2018-12-19 10:12:07 -0300

    Thinking in Java, Everything is a Object, exercise 12 (Modified exercise 2 to generate javadoc upon it)

--- Thinking in Java/Everything is a Object/src/exercise_2_EIAO/Exercise2.java
+++ Thinking in Java/Everything is a Object/src/exercise_2_EIAO/Exercise2.java
@@ -1,7 +1,14 @@
 package exercise_2_EIAO;
 
+/** "Thinking in Java" book, "Everything is a Object" chapter, exercises number 2 and 12 together.
+ * Display in the console the traditional "Hello World"
+ * @author Edgar Hernández
+ */
 public class Exercise2 {
 
+	/**Entry point method (Main)
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 	}
