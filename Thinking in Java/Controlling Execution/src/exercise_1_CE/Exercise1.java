32657e8e09828d468bbd06059b8a22b6fc8a7dae
Edgar Hernandez <riseofagamer@gmail.com>
2018-12-19 16:47:07 -0300

    Thinking in Java, Controlling Execution, Exercise 7 (Modified exercise 1)

--- Thinking in Java/Controlling Execution/src/exercise_1_CE/Exercise1.java
+++ Thinking in Java/Controlling Execution/src/exercise_1_CE/Exercise1.java
@@ -5,8 +5,14 @@ public class Exercise1
 	public static void main(String[] args) 
 	{
 		for(int i = 1; i <= 100; i++)
+		{
 			System.out.println(i);
-
+			
+			//Exercise 7 requirement
+			if(i == 99)
+				break;
+		}
+		
 	}
 
 }
